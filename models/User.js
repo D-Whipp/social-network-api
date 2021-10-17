716e25ad0ce0427203ea602314e4d4e76aece5d1
Mr Whipp <dwhipp88@gmail.com>
2021-10-17 16:06:39 -0600

    initial commit

---
+++ models/User.js
@@ -0,0 +1,27 @@
+const { Schema, model } = require("mongoose");
+const { isEmail } = require(validator);
+
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: [isEmail, "invalid email"],
+    },
+    thoughts: [{ type: Schema.Types.ObjectId, ref: "Thought" }],
+    friends: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
